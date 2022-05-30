c74c7091fc2e05ac1d66f58b3ca5f0334502471b
sbot <sbotejaru2@gmail.com>
2022-05-30 20:46:17 +0300

    -

---
+++ MVP_TEMA1/AboutWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Diagnostics;
+using System.Windows.Navigation;
+
+namespace MVP_TEMA1
+{
+    /// <summary>
+    /// Interaction logic for AboutWindow.xaml
+    /// </summary>
+    public partial class AboutWindow : Window
+    {
+        public AboutWindow()
+        {
+            InitializeComponent();
+        }
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {            
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+        }
+    }
+}
